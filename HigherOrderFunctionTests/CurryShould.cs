a21558fc2ce50f75d81b180b09f0f0d7e2b75b16
David Lewis <dalewi2@gmail.com>
2016-09-17 01:04:00 -0500

    added Curry Tests.

---
+++ HigherOrderFunctionTests/CurryShould.cs
@@ -0,0 +1,41 @@
+﻿using Functional;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace HigherOrderFunctionTests
+{
+	[TestClass]
+	public class CurryShould
+	{
+		[TestMethod]
+		public void ForArityTwoReturnAFuncOfFuncEtc()
+		{
+			var a1 = HigherOrderFunctions.Curry<string, int, bool>(TestArityTwo);
+			Assert.IsInstanceOfType(a1, typeof(Func<string, Func<int, bool>>));
+			Assert.IsTrue(a1("")(1));
+		}
+
+		[TestMethod]
+		public void ForArityThreeReturnAFuncOfFuncEtc()
+		{
+			var a1 = HigherOrderFunctions.Curry<string, int, bool, bool>(TestArityThree);
+			Assert.IsInstanceOfType(a1, typeof(Func<string, Func<int, Func<bool, bool>>>));
+			Assert.IsTrue(a1("")(1)(true));
+		}
+
+		private bool TestArityTwo(string a, int b)
+		{
+			return true;
+		}
+
+		private bool TestArityThree(string a, int b, bool c)
+		{
+			return true;
+		}
+
+		private bool TestArityFour(string a, int b, bool c, double d)
+		{
+			return true;
+		}
+	}
+}
